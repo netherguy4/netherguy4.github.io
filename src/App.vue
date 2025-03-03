9192a3c7e1fcf34349b828adc137bf38af54575c
netherguy4 <trawe1234@gmail.com>
2025-03-03 16:03:59 +0200

    build v2.2.1 | width fix

--- src/App.vue
+++ src/App.vue
@@ -209,7 +209,9 @@ onMounted(() => {
   }
   &__container {
     height: 100%;
-    // max-width: 800px;
+    max-width: 800px;
+    // width: 100%;
+    // height: 100%;
     margin: 0 auto;
   }
   &__content {
